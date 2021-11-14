635a75a74d167934e3b1d84b7470c01f688805fa
mark novelero <mark.novelero@gmail.com>
2021-11-14 07:46:33 -0500

    clean up

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 console.log('javascript connected')
 
 let userCollectionMain = document.querySelector('#main')
-let globalAllUsers = []
-let globalPosts = []
+
 
 window.onload = function getData(){
 
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
-    .then((allUsers) => { 
-      globalAllUsers = allUsers;  
+    .then((allUsers) => {  
 
       fetch('https://jsonplaceholder.typicode.com/posts')
-      .then(res => res.json())
+      .then(response => response.json())
       .then(posts => {
         allUsers.forEach((user) => {
             renderUser(user, posts) 
@@ -21,60 +19,55 @@ window.onload = function getData(){
  })
 }
 
-    
-
 function renderUser(user, posts){
-    
-    globalPosts = posts; 
+     
+    // Grab posts made by user. 
+    let userPosts = posts.filter(post => user.id === post.userId);
 
-    let userPosts = posts.filter(post => user.id === post.userId)
 
     let userDiv = document.createElement('div');
-    userDiv.className = "user-div"
+    userDiv.className = 'user-div';
 
     let userFullName = document.createElement('h2'); 
-    userFullName.className = `${user.name}`
-    userFullName.innerText= user.name
+    userFullName.className = `${user.name}`;
+    userFullName.innerText= user.name;
     
-
     let userName = document.createElement('h3'); 
-    userName.className = `${user.username}`
-    userName.innerText = `KSense account: ${user.username}`
+    userName.className = `${user.username}`;
+    userName.innerText = `KSense account: ${user.username}`;
     
-
     let seePostButton = document.createElement('button'); 
-    seePostButton.className = `see-post-button-${user.id}`
-    seePostButton.innerText = "View Posts"
+    seePostButton.className = `see-post-button-${user.id}`;
+    seePostButton.innerText = 'View Posts';
+    seePostButton.name = 'post-button';
+    seePostButton.type = 'button';
   
-    userDiv.append(userFullName, userName, seePostButton)
-    console.log(userDiv)
-    userCollectionMain.append(userDiv)
+    userDiv.append(userFullName, userName, seePostButton);
+    userCollectionMain.append(userDiv);
 
-    seePostButton.addEventListener("click", (e) => {
+
+    //Render posts onclick. 
+    seePostButton.addEventListener('click', (e) => {
         
-
         userPosts.forEach(post => {
 
-            let divPost = document.createElement('div')
-            divPost.className = 'div-post'
+            let divPost = document.createElement('div');
+            divPost.className = 'div-post';
             
-            let titlePost = document.createElement('h4')
-            titlePost.className = "user-post"
-            titlePost.innerText= post.title
+            let titlePost = document.createElement('h4');
+            titlePost.className = "user-post";
+            titlePost.innerText= post.title;
             
-            let postBody = document.createElement('p')
-            postBody.className = "post-body"
-            postBody.innerText = post.body
+            let postBody = document.createElement('p');
+            postBody.className = 'post-body';
+            postBody.innerText = post.body;
 
-            divPost.append(titlePost, postBody)
-            userDiv.append(divPost)
+            divPost.append(titlePost, postBody);
+            userDiv.append(divPost);
             
         })
     })
-
-    
-  
-}
+};
 
 
  
